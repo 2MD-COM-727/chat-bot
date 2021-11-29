4f98c7402f5f416b885f81b183fafe0e5e83e956
Darius <23092088+DariusJA@users.noreply.github.com>
2021-11-29 16:40:49 +0000

    Pylint fixes

--- chat_bot/training.py
+++ chat_bot/training.py
@@ -1,9 +1,20 @@
+"""
+
+"""
+
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
 
 def train(directory):
-    query_types = ["borrowing_laptop", "computer_booking", "ebooks", "location_of_books", "printing", "study_room"]
+    query_types = [
+        "borrowing_laptop",
+        "computer_booking",
+        "ebooks",
+        "location_of_books",
+        "printing",
+        "study_room",
+    ]
 
     training_lines = []
     for query_type in query_types:
@@ -15,14 +26,16 @@ def train(directory):
                     training_lines.append(answer)
 
     chatbot = ChatBot("Charlie")
-    trainer = ListTrainer(chatbot,
-                          logic_adapters=[
-                            {
-                                'import_path': 'chatterbot.logic.BestMatch',
-                                'default_response': 'I am sorry, but I do not understand.',
-                                'maximum_similarity_threshold': 0.7
-                            }
-                          ])
+    trainer = ListTrainer(
+        chatbot,
+        logic_adapters=[
+            {
+                "import_path": "chatterbot.logic.BestMatch",
+                "default_response": "I am sorry, but I do not understand.",
+                "maximum_similarity_threshold": 0.7,
+            }
+        ],
+    )
     trainer.train(training_lines)
 
     return chatbot
