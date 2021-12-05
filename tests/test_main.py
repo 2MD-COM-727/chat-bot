5233e7643f663bd9f340a9f1549e08b02682d6bc
Mikey Lau <mikey.lau@traininblocks.com>
2021-12-05 22:57:53 +0000

    feat(catalogue): Added basic catalogue search and webview.
    * Added some search functionality
    
    * Pylint fixes and better tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,20 @@
+"""Testing for catalogue module.
+
+Tests the following: finding a book, author, or subject with a single word search.
+
+Requires the following packages: unittest and chat_bot.
+"""
+
 import unittest
 from chat_bot.gui import ChatGUI
 
 
 class TestMain(unittest.TestCase):
+    """Test instance for the main module."""
+
     def test_gui_instance(self):
+        """Checks if a ChatGUi instance exists."""
+
         self.assertIsNotNone(ChatGUI)
 
 
