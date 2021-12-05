ff590a80de1c313d163240046b5af687956ad650
Mikey Lau <mikey.lau@traininblocks.com>
2021-12-05 22:55:08 +0000

    Pylint fixes and better tests

---
+++ tests/test_catalogue.py
@@ -0,0 +1,56 @@
+"""Testing for catalogue module.
+
+Tests the following: finding a book, author, or subject with a single word search.
+
+Requires the following packages: unittest and chat_bot.
+"""
+
+from unittest import TestCase, main as run_tests
+from unittest.mock import patch
+from chat_bot.catalogue import run
+
+class TestCatalogue(TestCase):
+    """Test instance for the catalogue module."""
+
+    @patch("builtins.input", side_effect=["1", "sport"])
+    # pylint: disable=unused-argument
+    def test_simple_book_search(self, mock_inputs):
+        """Tests search by title option with a single word."""
+
+        result = run()
+        target = (
+            "https://catalogue.solent.ac.uk/discovery/search?"
+            "query=title,contains,sport&tab=Combined&"
+            "search_scope=MyInstitution&vid=44SSU_INST:VU1&offset=0&mode=advanced"
+        )
+        self.assertEqual(result, target)
+
+    @patch("builtins.input", side_effect=["2", "McGill"])
+    # pylint: disable=unused-argument
+    def test_simple_author_search(self, mock_inputs):
+        """Tests search by author option with a single word."""
+
+        result = run()
+        target = (
+            "https://catalogue.solent.ac.uk/discovery/search?"
+            "query=creator,contains,McGill&tab=Combined&"
+            "search_scope=MyInstitution&vid=44SSU_INST:VU1&offset=0&mode=advanced"
+        )
+        self.assertEqual(result, target)
+
+    @patch("builtins.input", side_effect=["3", "maths"])
+    # pylint: disable=unused-argument
+    def test_simple_subject_search(self, mock_inputs):
+        """Tests search by subject option with a single word."""
+
+        result = run()
+        target = (
+            "https://catalogue.solent.ac.uk/discovery/search?"
+            "query=sub,contains,maths&tab=Combined&"
+            "search_scope=MyInstitution&vid=44SSU_INST:VU1&offset=0&mode=advanced"
+        )
+        self.assertEqual(result, target)
+
+
+if __name__ == "__main__":
+    run_tests()
--- tests/test_gui.py
+++ tests/test_gui.py
@@ -1,9 +1,20 @@
+"""Testing for gui module.
+
+Tests the following: the tkinter window and header instance/style.
+
+Requires the following packages: unittest and chat_bot.
+"""
+
 import unittest
 from chat_bot.gui import ChatGUI
 
 
 class TestGUI(unittest.TestCase):
+    """Test instance for the gui module."""
+
     def test_window(self):
+        """Tests the tkinter window style."""
+
         instance = ChatGUI()
         self.assertEqual(instance.WINDOW_TITLE, "Solent Library's ChatBot")
         self.assertEqual(instance.WINDOW_HEIGHT, 420)
@@ -11,6 +22,8 @@ class TestGUI(unittest.TestCase):
         self.assertEqual(instance.WINDOW_BG_COLOR, "#FFFFFF")
 
     def test_header_label(self):
+        """Tests the tkinter header style."""
+
         instance = ChatGUI()
         self.assertEqual(instance.HEADER_LABEL_BG, "#EE7A84")
         self.assertEqual(instance.HEADER_LABEL_FG, "#000000")
