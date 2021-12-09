160bd9d4febef4136e4afd58b18264be78b80ec0
Darius <23092088+DariusJA@users.noreply.github.com>
2021-12-09 22:17:43 +0000

    Updates to model.py

--- chat_bot/model.py
+++ chat_bot/model.py
@@ -76,13 +76,13 @@ def prepare_data():
 def build_model(size_x, size_y):
     # Neural network (NN) with dropouts to avoid overfitting.
     model = Sequential()
-    model.add(Dense(128, input_shape=(len(size_x),), activation="relu"))
+    model.add(Dense(128, input_shape=(size_x,), activation="relu"))
     model.add(Dropout(0.2))
     model.add(Dense(64, activation="relu"))
     model.add(Dropout(0.2))
 
     # Output layer has the same number of nodes as labels.
-    model.add(Dense(len(size_y), activation="softmax"))
+    model.add(Dense(size_y, activation="softmax"))
 
     # Sets the hyperparameters for the NN (optimizer).
     sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
@@ -97,41 +97,39 @@ def train_and_save_model(training):
     # Shuffles the training data and splits it into input (x) and output (y).
     shuffle(training)
     training = np.array(training, dtype=object)
-    train_x = list(training[:, 0])
-    train_y = list(training[:, 1])
+    X_train = list(training[:, 0])
+    y_train = list(training[:, 1])
 
-    model = build_model(train_x[0], train_y[0])
+    model = build_model(len(X_train[0]), len(y_train[0]))
 
     # Trains and saves the model
     trained_model = model.fit(
-        np.array(train_x), np.array(train_y), epochs=15, batch_size=5, verbose=1
+        np.array(X_train), np.array(y_train), epochs=12, batch_size=5, verbose=1
     )
     model.save("./data/chatbot_model.h5", trained_model)
 
 
 def evaluate_ttsplit(training, num_epochs):
-
     training = np.array(training, dtype=object)
     X = np.array([np.array(x) for x in training[:, 0]])
     y = np.array([np.array(x) for x in training[:, 1]])
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
 
-    model = build_model(X[0], y[0])
+    model = build_model(len(X[0]), len(y[0]))
     model.fit(X_train, y_train, epochs=num_epochs, batch_size=5, verbose=0)
 
     return model.evaluate(X_test, y_test, verbose=0)
 
 
 def evaluate_kfold(training, num_epochs):
-
     training = np.array(training, dtype=object)
     X = np.array([np.array(x) for x in training[:, 0]])
     y = np.array([np.array(x) for x in training[:, 1]])
 
     skf = StratifiedKFold(n_splits=6, shuffle=True, random_state=1)
 
-    model = build_model(X[0], y[0])
+    model = build_model(len(X[0]), len(y[0]))
 
     loss_scores = []
     accuracy_scores = []
