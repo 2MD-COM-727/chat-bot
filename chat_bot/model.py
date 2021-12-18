eb38623b16656930300cf4f04a6080968b99f133
maggie <9nungm38@solent.ac.uk>
2021-12-18 15:36:27 +0000

    started plotting graph

--- chat_bot/model.py
+++ chat_bot/model.py
@@ -198,3 +198,25 @@ class Model:
 
 model = Model()
 model.show_graph()
+
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+y = []
+
+plt.xlabel("x values")
+plt.ylabel("y values")
+
+plt.plot(x, y)
+plt.show()
+
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+y = []
+
+plt.xlabel("x values")
+plt.ylabel("y values")
+
+plt.plot(x, y)
+plt.show()
